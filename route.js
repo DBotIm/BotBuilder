854326f382005173f39f844c2da65d3ac2952006
skings <samanf74@gmail.com>
2018-02-17 14:47:18 +0330

    adding route.js as an server change first js must run to route.js

---
+++ route.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Create a server with a host and port
+const server = Hapi.server({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, h) {
+
+        return 'hello world';
+    }
+});
