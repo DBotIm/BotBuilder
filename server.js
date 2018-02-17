3eceb34fc78cfd3b58e3a4a55310343a5c57e801
skings <samanf74@gmail.com>
2018-02-17 14:48:20 +0330

    change name of route.js to server.js

--- server.js
+++ server.js
@@ -15,3 +15,19 @@ server.route({
         return 'hello world';
     }
 });
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
