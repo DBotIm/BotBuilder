d4eb0222dd9875c078ad2ed8c9d499bdb153be52
skings <samanf74@gmail.com>
2018-02-17 15:13:34 +0330

    fixing

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var Hapi = require('hapi');
+const Hapi = require('hapi');
 
 // Create a server with a host and port
 const server = Hapi.server({
     host: 'localhost',
-    port: 8000
+    port: 3000
 });
 
 // Add the route
